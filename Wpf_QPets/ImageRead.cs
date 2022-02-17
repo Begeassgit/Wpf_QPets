123aa51ac226fad7e7d5c927781574244f44a74e
Begeassgit <begeass@sina.com>
2022-02-17 23:45:08 +0800

    完成基本显示和右键切换功能 例示干员：年

---
+++ Wpf_QPets/ImageRead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Wpf_QPets
+{
+    public class ReadImageName
+    {
+        public string[] GetImageNames()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"Image");
+            int imageCount = directoryInfo.GetFiles().Length;
+            string[] imageName = new string[imageCount];
+            for (int i = 0; i < imageCount; i++)
+            {
+                imageName[i]=directoryInfo.GetFiles()[i].Name;
+            }
+
+            return imageName;
+        }
+    }
+}
--- Wpf_QPets/MainWindow.xaml.cs
+++ Wpf_QPets/MainWindow.xaml.cs
@@ -10,8 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using WpfAnimatedGif;
+
 
 namespace Wpf_QPets
 {
@@ -20,9 +24,60 @@ namespace Wpf_QPets
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string temp;
         public MainWindow()
         {
             InitializeComponent();
+            this.AllowsTransparency = true;
+            MyAnimation("relax.gif");
+        }
+
+        internal void MyAnimation(string type)
+        {
+            BitmapImage image = new BitmapImage();
+            ReadImageName readImageName = new ReadImageName();
+            foreach(string name in readImageName.GetImageNames())
+            {
+                if (name == type)
+                {
+                    image.BeginInit();
+                    image.UriSource = new Uri(@"Image\" + type, UriKind.Relative);
+                    image.EndInit();
+                    ImageBehavior.SetAnimatedSource(Body, image);
+                    ImageBehavior.SetRepeatBehavior(Body, RepeatBehavior.Forever);
+                }
+                
+            }    
+        }
+        private void RelaxClick(object sender, RoutedEventArgs e)
+        {
+            MyAnimation("relax.gif");
+        }
+
+        private void SitClick(object sender, RoutedEventArgs e)
+        {
+            MyAnimation("sit.gif");
+        }
+
+        private void SleepClick(object sender, RoutedEventArgs e)
+        {
+            MyAnimation("sleep.gif");
+        }
+        private void SpecialClick(object sender, RoutedEventArgs e)
+        {
+            MyAnimation("special.gif");
+        }
+
+        private void Body_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            temp = ImageBehavior.GetAnimatedSource(Body).ToString();
+            MyAnimation("interact.gif");          
+        }
+
+        private void Body_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MyAnimation(temp.Substring(6));
         }
     }
 }
